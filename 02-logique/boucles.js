dfcd5f5482d01f9cdca27835f7a5354e74959d0f
Bekoin <bekoinbekoin0@gmail.com>
2026-04-10 20:39:15 +0000

    Ajout de l'exercice sur l'utilisation de while, for..in et for..of en javascript.

--- 02-logique/boucles.js
+++ 02-logique/boucles.js
@@ -13,3 +13,37 @@ console.log("------------------------------");
     console.log("Le fruit est:" + fruits[l]);
  }
 console.log("------------------------------");
+
+//02. Exercice sur l'utilisation de la boucle while
+//Exercice1 : Afficher les nombres paires de 0 à 20
+let h = 0;
+while(h <= 20){
+    console.log("Le nommbre est: " + h);
+    h += 2 // incrémentation de 2 pour obtenir les nombres paires
+    }
+console.log("------------------------------");
+
+//Exercice2: Afficher les éléments d'un tableau avec while
+let caractéristiquesDéveloppeur = ["Créatif", "Analytique", "Curieux", "Persévérant"];
+let j = 0;
+while(j < caractéristiquesDéveloppeur.length){
+    console.log("La caractéristique du développeur : " + caractéristiquesDéveloppeur[j]);
+    j++; // incrémentation pour passer à l'élément suivant du tableau
+}
+console.log("------------------------------");
+
+//03. Utilisation de la boucle for..in
+let salutations = "Bonjour! ";
+for(let letter in salutations){
+    console.log(letter);
+}
+
+console.log("------------------------------");
+
+//04. Utilisation de for ..of
+let salutations2 = "Bonsoir! ";
+for(let letter of salutations2){
+    console.log(letter);
+}
+
+Console.log("------------------------------");
