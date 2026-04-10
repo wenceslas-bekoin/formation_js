ecc34d9197afc8727a8ca80b465f1651d9611085
Bekoin <bekoinbekoin0@gmail.com>
2026-04-10 20:00:02 +0000

    Ajout de l'exercice sur les boucles en javacsript.

--- 02-logique/boucles.js
+++ 02-logique/boucles.js
@@ -1,1 +1,15 @@
+//Les boucles en Javascript
 
+//01. Utilisation de la boucle for
+//Exercice1 : Afficher les nombres de 1 à 15
+for(let i = 1; i <= 15; i++){
+    console.log("Le nombre est: " + i);
+}
+console.log("------------------------------");
+
+ //Exercice2 : Parcourir un tableau et afficher chaque élément
+ let fruits = ["Mangue", "Banane", "Pomme", "Pamplemousse", "Ananas", "Avocat"];
+ for(let l = 0; l < fruits.length; l++){
+    console.log("Le fruit est:" + fruits[l]);
+ }
+console.log("------------------------------");
