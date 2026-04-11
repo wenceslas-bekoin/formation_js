82c20b3fd8e5310dfd03a4b10ceeb718aa9088d5
Bekoin <bekoinbekoin0@gmail.com>
2026-04-11 12:12:13 +0000

    Mini projet de générateur de citation en console.

---
+++ projets/générateur-de-citations/index.js
@@ -0,0 +1,20 @@
+// Mini projet de générateur de citations dans la console.
+
+const citations = [
+    "être sage, c'est de mériter le nom d'être humain. - Nichiren Daishonin.",
+    "La vie est le plus grand de tous les biens. - Nihiren Daishonin.",
+    "La boddhéité consite à progresser sans cesse. - Daisaku Ikeda.",
+    "On peut perdre la richesse matérielle, mais la sagesse ne disparaît jamais. - Proverbe des Philipines.",
+    "L'amitié est une force. - Daisaku Ikeda.",
+    "Il y a des choses plus importantes à mes yeux que moi-même. C'est l'humanité. - André Gide.",
+    "Il est possible de faire d'une défaite la cause d'une victoire future. - Josei Toda.",
+    "L'enfer est dans le coeur de celui qui néglige sa mère et méprise son père. - Nichiren Daishonin.",
+    
+];
+
+//générer un indexe aléatoire 
+const indexAléatoire = Math.floor(Math.random() * citations.length);
+
+//afficher la citationaleatoire
+console.log("\n Citation du jour : \n");
+console.log(citations[indexAléatoire]);
